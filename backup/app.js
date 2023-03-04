54c5527fbe0cd11c49847576b6e80b6939d82f2d
christianbauer4 <mrchristianbauerusa@gmail.com>
2023-03-04 13:06:18 -0500

    idk

--- backup/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', function (req, res) {
-	res.sendFile('index.html', {root: '../client/views'})
-})
-
-app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
-
