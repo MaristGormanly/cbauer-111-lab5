6f2fcc55c0502351f6d7459044a590699cae550a
christianb4 <mrchristianbauerusa@gmail.com>
2023-03-21 23:50:23 -0400

    adding some JS and other changes

--- client/public/js/countingCharacters.js
+++ client/public/js/countingCharacters.js
@@ -1,5 +1,5 @@
+/* function to count and limit amout of characters typed */
 var comments = document.getElementById('comments');
-
 comments.addEventListener('keyup', function(e) {
     var count = document.getElementById('comments').value.length;
     document.getElementById('count').value = count;
@@ -8,8 +8,23 @@ comments.addEventListener('keyup', function(e) {
     }
 });
 
+/* for liking images and counting the amount of likes */
 var like = document.getElementById('like');
 var likes = document.getElementById('likes');
 like.addEventListener('click', function(e) {
     likes.innerHTML = (Number(likes.innerHTML) + 1) + '';
 }); 
+
+/* for enlarging an image when clicked on */
+var img = document.getElementById('smallPic');
+img.addEventListener('click', function(e) {
+    img.style.transform = "scale(2)"
+    img.style.transition = "transform 0.25s ease";
+})
+
+/* resets enlarged image to original size */
+var resetImg = document.getElementById('resetImg');
+resetImg.addEventListener('click', function(e) {
+    img.style.transform = "scale(1)";
+    img.style.transition = "transform 0.25s ease";
+})
